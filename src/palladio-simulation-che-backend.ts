b47b1ab0947106a277a70575ef21a3add4d0cac9
ZhentangHe <he.zhentang@qq.com>
2019-12-11 12:59:08 +0100

    HelloWorld-ish command

--- src/palladio-simulation-che-backend.ts
+++ src/palladio-simulation-che-backend.ts
@@ -7,6 +7,16 @@ import * as theia from '@theia/plugin';
 
 export function start(context: theia.PluginContext) {
     console.log(theia.window.state);
+    //Start Palladio Simulation Command
+    const informationMessageTestCommand = {
+        id: 'palladio-simulation-start',
+        label: "Palladio: Start Simulation"
+    };
+    context.subscriptions.push(
+        theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
+            theia.window.showInformationMessage('Simulation Started! So wait!');
+        })
+    );
 }
 
 export function stop() {
