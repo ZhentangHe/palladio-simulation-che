89530cf194eab1bbbabb438c4c04759e7c106f13
ZhentangHe <he.zhentang@qq.com>
2019-12-16 18:20:24 +0100

    add how to run the sim in specific container

--- src/palladio-simulation-che-backend.ts
+++ src/palladio-simulation-che-backend.ts
@@ -8,13 +8,21 @@ import * as theia from '@theia/plugin';
 export function start(context: theia.PluginContext) {
     console.log(theia.window.state);
     //Start Palladio Simulation Command
-    const informationMessageTestCommand = {
-        id: 'palladio-simulation-start',
+    const palladioSimulationStartCommand = {
+        id: 'palladio.start',
         label: "Palladio: Start Simulation"
     };
     context.subscriptions.push(
-        theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
-            theia.window.showInformationMessage('Simulation Started! So wait!');
+        theia.commands.registerCommand(palladioSimulationStartCommand, (...args: any[]) => {
+            theia.window.showInformationMessage('palladio simulation started');
+            //select which container to run
+            theia.commands.executeCommand('terminal-in-specific-container:new');
+            //dummy, not encapsulated impl.
+            theia.window.onDidOpenTerminal(async (openedTerminal: theia.Terminal) => {
+                const openedTerminalId = await openedTerminal.processId;
+                openedTerminal.sendText('clear && echo Palladio Simulation started.\n');
+                openedTerminal.sendText('touch test.csv && ls');               
+            })
         })
     );
 }
